9921fc288cad76215735847d428e249025234a5b
juliemovify <julie@movify.com>
2019-04-15 11:07:21 +0200

    Add redux and redux saga

---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./reducers";
+import rootSaga from "./sagas";
+import createSagaMiddleware from "redux-saga";
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
