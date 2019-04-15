f680e1f4f9a7a45ea79796226ad156e867eab7e3
juliemovify <julie@movify.com>
2019-04-15 11:07:45 +0200

    Add api util

---
+++ src/utils/api.js
@@ -0,0 +1,68 @@
+import { isEmpty, isNil } from "ramda";
+
+const DEFAULT_HEADERS = {
+  "Content-Type": "application/json"
+};
+
+const DEFAULT_BASE_URL = process.env.REACT_APP_BASE_URL;
+
+const makeBaseUrl = () => {
+  var cache = DEFAULT_BASE_URL;
+  return baseUrl => {
+    if (!isNil(baseUrl) && !isEmpty(baseUrl)) cache = baseUrl;
+    return cache;
+  };
+};
+
+const getBaseUrl = makeBaseUrl();
+
+export const setBaseUrl = url => makeBaseUrl(url);
+
+const makeHeaders = () => {
+  var cache = DEFAULT_HEADERS;
+  return (headers = {}) => {
+    cache = { ...cache, ...headers };
+    return cache;
+  };
+};
+
+const getHeaders = makeHeaders();
+
+export const setToken = token => getHeaders({ Authorization: token });
+
+const handleError = res => {
+  try {
+    return res.json();
+  } catch (error) {
+    return res;
+  }
+};
+
+const formatQueryParams = params => {
+  const queryParams = [];
+  for (var key in params) {
+    if (params.hasOwnProperty(key)) {
+      const value = params[key];
+      if (!isNil(value) && !isEmpty(value)) queryParams.add(`${key}=${value}`);
+    }
+  }
+  return !isEmpty(queryParams) ? `?${queryParams.join("&")}` : "";
+};
+
+export const get = (url, params = {}, headers = {}) => {
+  const queryParams = formatQueryParams(params);
+  return request(url + queryParams, {
+    method: "GET",
+    headers: { ...getHeaders(), ...headers }
+  });
+};
+
+export const post = (url, body = {}, headers = {}) =>
+  request(url, {
+    method: "POST",
+    headers: { ...getHeaders(), ...headers },
+    body: JSON.stringify(body)
+  });
+
+const request = (url, requestParams = {}) =>
+  fetch(getBaseUrl() + url, requestParams).then(handleError);
