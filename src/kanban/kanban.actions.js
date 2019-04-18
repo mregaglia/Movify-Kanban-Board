3c3a39241f82d3edd68971d228c6b64c6a20f8c6
juliemovify <julie@movify.com>
2019-04-18 11:55:09 +0200

    Add auth actions

---
+++ src/kanban/kanban.actions.js
@@ -0,0 +1,15 @@
+export const GET_KANBAN = "GET_KANBAN";
+export const UPDATE_KANBAN = "UPDATE_KANBAN";
+export const GET_JOB_ORDERS = "GET_JOB_ORDERS";
+export const GET_JOB_SUBMISSIONS = "GET_JOB_SUBMISSIONS";
+
+export const getKanban = () => ({ type: GET_KANBAN });
+export const updateKanban = kanban => ({
+  type: UPDATE_KANBAN,
+  payload: kanban
+});
+export const getJobOrders = bmId => ({ type: GET_JOB_ORDERS, payload: bmId });
+export const getJobSubmissions = (bmId, jobOrderId) => ({
+  type: GET_JOB_SUBMISSIONS,
+  payload: { bmId, jobOrderId }
+});
