71509ae592c0b5d80142d8058b6e3e8fb6289931
juliemovify <julie@movify.com>
2019-04-18 14:58:43 +0200

    Display data

--- src/kanban/Kanban.js
+++ src/kanban/Kanban.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { pathOr } from "ramda";
+import { pathOr, propOr } from "ramda";
 import { array, bool, func } from "prop-types";
 import { getKanban } from "./kanban.actions";
 
@@ -9,12 +9,36 @@ const Kanban = ({ getKanban, kanban, loading }) => {
     getKanban();
   }, []);
 
-  console.log(kanban);
-
   return (
     <div>
       {loading && <p>loading</p>}
       <p>Kanban</p>
+
+      {kanban.map(bm => (
+        <div key={bm.id}>
+          <span>{`${bm.firstName} ${bm.lastName} `}</span>
+          <p />
+          {propOr([], "jobOrders", bm).map(jobOrder => (
+            <div key={jobOrder.id}>
+              <span>{`${jobOrder.clientCorporation.name} ${jobOrder.title} ${
+                jobOrder.clientContact.firstName
+              } ${jobOrder.clientContact.lastName} `}</span>
+              <span>
+                {propOr([], "jobSubmissions", jobOrder)
+                  .map(
+                    jobSubmission =>
+                      `${jobSubmission.candidate.firstName} ${
+                        jobSubmission.candidate.lastName
+                      } `
+                  )
+                  .join(" - ")}
+              </span>
+            </div>
+          ))}
+          <br />
+          <br />
+        </div>
+      ))}
     </div>
   );
 };
