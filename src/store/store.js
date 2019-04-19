4f852953f03c32666277c48d5b5257b0ef5318d7
juliemovify <julie@movify.com>
2019-04-19 10:25:59 +0200

    Add redux devtools support

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,15 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import rootSaga from "./sagas";
 import createSagaMiddleware from "redux-saga";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
 
 sagaMiddleware.run(rootSaga);
 
