c13c1fca4dbe9f65a81dc5e07fbe555bb6afef72
juliemovify <julie@movify.com>
2019-04-19 10:26:17 +0200

    Add refresh token in storage

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,9 +1,17 @@
 const STORAGE_TOKEN = "STORAGE_TOKEN";
+const STORAGE_REFRESH_TOKEN = "STORAGE_REFRESH_TOKEN";
 const STORAGE_BASE_URL = "STORAGE_BASE_URL";
 
 export const getToken = () => localStorage.getItem(STORAGE_TOKEN);
 export const setToken = token => localStorage.setItem(STORAGE_TOKEN, token);
 export const removeToken = () => localStorage.removeItem(STORAGE_TOKEN);
+
+export const getRefreshToken = () =>
+  localStorage.getItem(STORAGE_REFRESH_TOKEN);
+export const setRefreshToken = refreshToken =>
+  localStorage.setItem(STORAGE_REFRESH_TOKEN, refreshToken);
+export const removeRefreshToken = () =>
+  localStorage.removeItem(STORAGE_REFRESH_TOKEN);
 
 export const getBaseUrl = () => localStorage.getItem(STORAGE_BASE_URL);
 export const setBaseUrl = baseUrl =>
