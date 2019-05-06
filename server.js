bbbfc79f796295522b9c49116e83850472c09a0f
juliemovify <julie@movify.com>
2019-05-06 10:54:22 +0200

    Add morgan logger

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const path = require("path");
+const morgan = require("morgan");
+
 const app = express();
+
+app.use(morgan("dev"));
 
 app.use(express.static(path.join(__dirname, "app", "build")));
 
