8962909b88c5d577b54ed68019145ec326980d7a
juliemovify <julie@movify.com>
2019-05-06 11:03:35 +0200

    Disable redux devtools on production

--- app/src/store/store.js
+++ app/src/store/store.js
@@ -3,7 +3,10 @@ import rootReducer from "./reducers";
 import rootSaga from "./sagas";
 import createSagaMiddleware from "redux-saga";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  (process.env.NODE_ENV === "development" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
