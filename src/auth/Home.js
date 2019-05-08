4fd924e8b899f364f1b0c664f7485d39182935dd
juliemovify <julie@movify.com>
2019-05-08 11:34:28 +0200

    Add logo in splash

--- src/auth/Home.js
+++ src/auth/Home.js
@@ -3,12 +3,25 @@ import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { path } from "ramda";
 import { bool, func } from "prop-types";
+import styled from "styled-components";
+
+const Container = styled.div({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  marginTop: "20%"
+});
 
 const Home = ({ authenticated, isCheckingAuth }) => {
   if (!isCheckingAuth && authenticated) return <Redirect to="/kanban" />;
   else if (!isCheckingAuth && authenticated === false)
     return <Redirect to="/login" />;
-  else return <div>splash</div>;
+  else
+    return (
+      <Container>
+        <img alt="movify" src={require("../assets/movify.png")} />
+      </Container>
+    );
 };
 
 Home.propTypes = {
