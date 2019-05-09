46f3a312bde2671469f84095a7f248efdb0c9199
juliemovify <julie@movify.com>
2019-05-09 14:51:12 +0200

    Add PriorityBadge component

---
+++ src/components/PriorityBadge.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { string } from "prop-types";
+import styled from "styled-components";
+
+const getColorFromPriority = priority => {
+  switch (priority) {
+    case "P1":
+      return "red";
+    case "P2":
+      return "orange";
+    case "P3":
+      return "yellow";
+    case "P4":
+      return "green";
+  }
+};
+
+const Badge = styled.div(({ color, theme }) => ({
+  backgroundColor: theme.colors[color],
+  height: 20,
+  width: 20,
+  borderRadius: 10
+}));
+
+const Text = styled.div(({ theme }) => ({
+  display: "inline-block",
+  fontFamily: theme.fonts.fontFamily,
+  fontSize: theme.textDimensions.small,
+  fontWeight: 600,
+  color: theme.colors.darkWhite,
+  padding: 4
+}));
+
+const PriorityBadge = ({ priority }) => (
+  <Badge color={getColorFromPriority(priority)}>
+    <Text>{priority}</Text>
+  </Badge>
+);
+
+PriorityBadge.propTypes = {
+  priority: string
+};
+
+export default PriorityBadge;
