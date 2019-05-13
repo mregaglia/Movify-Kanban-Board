d2a398d94a9d00e72c4c68a1115d20fe65229439
juliemovify <julie@movify.com>
2019-05-13 15:36:50 +0200

    Add form label component

---
+++ src/components/form/Label.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { string } from "prop-types";
+import styled from "styled-components";
+
+const StyledLabel = styled.div(({ theme }) => ({
+  color: theme.colors.red,
+  fontFamily: theme.fonts.fontFamily,
+  fontSize: theme.textDimensions.regular,
+  marginRight: 8,
+  minWidth: 80,
+  maxWidth: 80,
+  width: 80
+}));
+
+export const Label = ({ label }) => <StyledLabel>{label}</StyledLabel>;
+
+Label.propTypes = {
+  label: string
+};
+
+export default Label;
