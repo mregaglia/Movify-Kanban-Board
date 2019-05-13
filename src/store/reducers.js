d926c355c084ed65e2671564d4212ad78dd0c19e
juliemovify <julie@movify.com>
2019-05-13 13:52:44 +0200

    Add redux form dependency

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
+import { reducer as form } from "redux-form";
 import auth from "../auth/auth.reducer";
 import kanban from "../kanban/kanban.reducer";
 import departmentFilter from "../kanban/departmentFilter/departmentFilter.reducer";
 
-export default combineReducers({ auth, kanban, departmentFilter });
+export default combineReducers({ form, auth, kanban, departmentFilter });
