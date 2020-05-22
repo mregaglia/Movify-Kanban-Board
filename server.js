4f60e454b95095fd60643eafc24774604f8188d3
juliemovify <julie@movify.com>
2020-05-22 20:55:04 +0200

    Handle login with query in server

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
+const cors = require('cors');
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(cors({
+  origin: true
+}));
 
 app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/login", function(req, res) {
+  if (req != undefined && req.query != undefined && req.query.code != undefined) {
+    res.send(req.query)
+  } else {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  }
+});
 
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
