f82bf0d52515ca5c5614c4280c187e3b840f0980
augustinmovify <augustin@movify.com>
2020-10-13 14:56:20 +0200

    Add employee service to retrieve business manager and talent acquisition

---
+++ src/reporting/employees.sagas.js
@@ -0,0 +1,26 @@
+
+import { call, put, takeLatest } from "redux-saga/effects";
+import {
+    getBusinessManagerAndSourcingOfficer
+} from "./employees.service"
+
+import {
+    GET_EMPLOYEES,
+    setEmployees
+} from './employees.actions'
+
+export function* getEmployees() {
+    try {
+        const employees = yield call(getBusinessManagerAndSourcingOfficer);
+        console.log(employees)
+        yield put(setEmployees(employees))
+    } catch (e) {
+        //
+    }
+}
+
+export default function employeeSagas() {
+    return [
+        takeLatest(GET_EMPLOYEES, getEmployees),
+    ];
+}
