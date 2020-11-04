b090abc254b6c4b4a93acf99c64d5a4ef88139ad
augustinmovify <augustin@movify.com>
2020-11-04 14:45:41 +0100

    Correct & from getBusinessManagerAndSourcingOfficer

--- src/reporting/employees/employees.service.js
+++ src/reporting/employees/employees.service.js
@@ -3,9 +3,9 @@ import { get } from "../../utils/api";
 export const getBusinessManagerAndSourcingOfficer = (start = 0) =>
     get("query/CorporateUser", {
         fields: "id,firstName,lastName,occupation,primaryDepartment",
-        where: "(occupation IN ('Reporting Owner & Business Manager', 'Business Manager & Reporting Owner',"
-            + "'Reporting Owner & Sourcing Officer', 'Sourcing Officer & Reporting Owner', 'Talent Acquisition & Reporting Owner',"
-            + "'Reporting Owner & Talent Acquisition', 'Business Manager', 'Sourcing Officer', 'Talent Acquisition' ) AND isDeleted=false",
+        where: "(occupation IN ('Reporting Owner %26 Business Manager', 'Business Manager %26 Reporting Owner',"
+            + "'Reporting Owner %26 Sourcing Officer', 'Sourcing Officer %26 Reporting Owner', 'Talent Acquisition %26 Reporting Owner',"
+            + "'Reporting Owner %26 Talent Acquisition', 'Business Manager', 'Sourcing Officer', 'Talent Acquisition' ) AND isDeleted=false",
         orderBy: "firstName,primaryDepartment.name",
         start: 0
     });
