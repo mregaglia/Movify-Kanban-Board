511e0c0e7178c200c04b9600d69fedf41f837d39
augustinmovify <augustin@movify.com>
2020-11-04 14:40:45 +0100

    Update retrieving role for employee

--- src/reporting/employees/employees.service.js
+++ src/reporting/employees/employees.service.js
@@ -3,7 +3,9 @@ import { get } from "../../utils/api";
 export const getBusinessManagerAndSourcingOfficer = (start = 0) =>
     get("query/CorporateUser", {
         fields: "id,firstName,lastName,occupation,primaryDepartment",
-        where: "(occupation='Business Manager' OR occupation='Sourcing Officer') AND isDeleted=false",
+        where: "(occupation IN ('Reporting Owner & Business Manager', 'Business Manager & Reporting Owner',"
+            + "'Reporting Owner & Sourcing Officer', 'Sourcing Officer & Reporting Owner', 'Talent Acquisition & Reporting Owner',"
+            + "'Reporting Owner & Talent Acquisition', 'Business Manager', 'Sourcing Officer', 'Talent Acquisition' ) AND isDeleted=false",
         orderBy: "firstName,primaryDepartment.name",
         start: 0
     });
