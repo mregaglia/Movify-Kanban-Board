4a427d12e599eb4521a40507616747470b0b7be8
augustinmovify <augustin@movify.com>
2020-12-10 10:16:32 +0100

    Displaying Interview done data for expand view

--- src/reporting/components/TalentAcquisition.js
+++ src/reporting/components/TalentAcquisition.js
@@ -10,7 +10,8 @@ import {
 } from '../../auth/user.sagas'
 import {
     LINKED_INMAIL,
-    INTERVIEW_SCHEDULED
+    INTERVIEW_SCHEDULED,
+    INTERVIEW_DONE
 } from '../expandView/expandView.sagas'
 import {
     FIRST_WEEK, SECOND_WEEK, FOURTH_WEEK, THIRD_WEEK
@@ -51,11 +52,17 @@ const TalentAcquisition = ({ datas, occupation }) => {
                     } else if ((datas[key].TITLE === LABEL_INTERVIEW_DONE && !occupation.includes(SOURCING_OFFICER))) {
                         return (
                             <TableContentTbodyTr key={i}>
-                                <TableContentTdLabelBold>{datas[key].TITLE}</TableContentTdLabelBold>
-                                <TableContentTdBoldClickable>{datas[key].FIRST_WEEK}</TableContentTdBoldClickable>
-                                <TableContentTdBoldClickable>{datas[key].SECOND_WEEK}</TableContentTdBoldClickable>
-                                <TableContentTdBoldClickable>{datas[key].THIRD_WEEK}</TableContentTdBoldClickable>
-                                <TableContentTdBoldClickable>{datas[key].FOURTH_WEEK}</TableContentTdBoldClickable>
+                                <TableContentTdLabelBold >{datas[key].TITLE}</TableContentTdLabelBold>
+                                {
+                                    tableWeek.map((week) =>
+                                        <TableContentTdBoldClickable key={week} data-for={i + '' + key + '' + week} data-event="click" data-tip>{datas[key][week]}
+                                            <ReactTooltip id={i + '' + key + '' + week} globalEventOff='click' place="right" clickable={true}>
+                                                <ExpandViewDetailCandidates week={week} title={INTERVIEW_DONE} />
+                                            </ReactTooltip>
+                                        </TableContentTdBoldClickable>
+                                    )
+                                }
+
                             </TableContentTbodyTr>
                         )
                     } else if ((datas[key].TITLE === LABEL_INTERVIEW_SCHEDULE && !occupation.includes(BUSINESS_MANAGER))) {
