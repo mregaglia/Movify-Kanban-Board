3a761577153a66c166e9b801fbc9fe2c0532e027
Lars Follet <lars.follet@movify.com>
2021-04-19 13:44:15 +0200

    Changle label ITV1 to Backlog

--- src/components/board/Column.js
+++ src/components/board/Column.js
@@ -1,16 +1,24 @@
-import React from "react";
-import { array, string } from "prop-types";
-import styled from "styled-components";
-import { Droppable } from "react-beautiful-dnd";
-import { STATUS_NO_GO } from "../../utils/kanban";
-import Candidates from "./Candidates";
+import React from "react"
+import { array, string } from "prop-types"
+import styled from "styled-components"
+import { Droppable } from "react-beautiful-dnd"
+import {
+  STATUS_NO_GO,
+  STATUS_ITV1,
+  STATUS_ITV2,
+  STATUS_TO_SEND,
+  STATUS_WF_RESPONSE,
+  STATUS_INTAKE,
+  STATUS_WF_FEEDBACK,
+} from "../../utils/kanban"
+import Candidates from "./Candidates"
 
 const getBackgroundColor = (isNoGo, snapshot, theme) => {
-  if (snapshot.isDraggingOver) return theme.colors.transparentRed;
-  if (snapshot.draggingFromThisWith) return theme.colors.transparentGrey;
-  if (isNoGo) return theme.colors.lightGrey;
-  return theme.colors.darkWhite;
-};
+  if (snapshot.isDraggingOver) return theme.colors.transparentRed
+  if (snapshot.draggingFromThisWith) return theme.colors.transparentGrey
+  if (isNoGo) return theme.colors.lightGrey
+  return theme.colors.darkWhite
+}
 
 const Container = styled.div(({ columnWidth }) => ({
   display: "flex",
@@ -18,8 +26,8 @@ const Container = styled.div(({ columnWidth }) => ({
   flexGrow: 1,
   width: columnWidth,
   minWidth: columnWidth,
-  maxWidth: columnWidth
-}));
+  maxWidth: columnWidth,
+}))
 
 const Content = styled.div(({ isNoGo, snapshot, theme }) => ({
   display: "flex",
@@ -30,8 +38,8 @@ const Content = styled.div(({ isNoGo, snapshot, theme }) => ({
   padding: 8,
   margin: 4,
   backgroundColor: getBackgroundColor(isNoGo, snapshot, theme),
-  borderRadius: theme.dimensions.borderRadius
-}));
+  borderRadius: theme.dimensions.borderRadius,
+}))
 
 const Title = styled.div(({ theme }) => ({
   fontFamily: theme.fonts.fontFamily,
@@ -40,13 +48,23 @@ const Title = styled.div(({ theme }) => ({
   paddingTop: 4,
   paddingBottom: 4,
   textOverflow: "ellipsis",
-  overflow: "hidden"
-}));
+  overflow: "hidden",
+}))
+
+const getStatusLabel = new Map([
+  [STATUS_ITV1, 'Backlog'],
+  [STATUS_ITV2, STATUS_ITV2],
+  [STATUS_TO_SEND, STATUS_TO_SEND],
+  [STATUS_WF_RESPONSE, STATUS_WF_RESPONSE],
+  [STATUS_INTAKE, STATUS_INTAKE],
+  [STATUS_WF_FEEDBACK, STATUS_WF_FEEDBACK],
+  [STATUS_NO_GO, STATUS_NO_GO],
+])
 
 const Column = ({ board, columnId, columnWidth, jobSubmissions, status }) => {
   return (
     <Container columnWidth={columnWidth}>
-      <Title>{status}</Title>
+      <Title>{getStatusLabel.get(status)}</Title>
       <Droppable droppableId={columnId}>
         {(provided, snapshot) => (
           <Content snapshot={snapshot} isNoGo={status === STATUS_NO_GO}>
@@ -62,13 +80,13 @@ const Column = ({ board, columnId, columnWidth, jobSubmissions, status }) => {
         )}
       </Droppable>
     </Container>
-  );
-};
+  )
+}
 Column.propTypes = {
   board: string,
   columnId: string,
   columnWidth: string,
   jobSubmissions: array,
-  status: string
-};
-export default Column;
+  status: string,
+}
+export default Column
