741acb166607c89dca83f9fd13bbbfa7e78b05b8
Lars Follet <lars.follet@movify.com>
2021-04-21 17:04:07 +0200

    Remove comment, add count to fetch

--- src/hooks/useHotCandidates.js
+++ src/hooks/useHotCandidates.js
@@ -1,41 +1,13 @@
 import { useQuery } from "react-query"
 import { get } from "../utils/api"
 
-// https://rest.bullhornstaffing.com/rest-services/e999/entity/Candidate/123,456?fields=id,firstName,lastName
-// https://rest.bullhornstaffing.com/rest-services/e999/entity/Candidate/11997,14101?fields=id,firstName,lastName,dateAvailable,occupation,status
-// const useHotCandidates = (candidateIds) =>
-//   useQuery(
-//     ["find-hot-candidates", candidateIds?.join()],
-//     () =>
-//       get('query/Candidate', {
-//         fields: "id,firstName,lastName,dateAvailable,occupation,submissions,owner",
-//         where: `id IN (${candidateIds?.join()})`
-//         // query: `id:${candidateIds?.join(" ")}`,
-//       }),
-//     {
-//       // Enable once we have candidates
-//       enabled: !!candidateIds?.length,
-//     }
-//   )
-// const useHotCandidates = (candidateIds) =>
-//   useQuery(
-//     ["find-hot-candidates", candidateIds?.join()],
-//     () =>
-//       get('search/Candidate', {
-//         fields: "id,firstName,lastName,dateAvailable,occupation,submissions,owner",
-//         query: `id:${candidateIds?.join(" ")}`,
-//       }),
-//     {
-//       // Enable once we have candidates
-//       enabled: !!candidateIds?.length,
-//     }
-//   )
 const useHotCandidates = (candidateIds) =>
   useQuery(
     ["find-hot-candidates", candidateIds?.join()],
     () =>
       get(`entity/Candidate/${candidateIds?.join()}`, {
         fields: "id,firstName,lastName,dateAvailable,occupation,submissions,owner",
+        count: candidateIds?.length || 0
       }),
     {
       // Enable once we have candidates
